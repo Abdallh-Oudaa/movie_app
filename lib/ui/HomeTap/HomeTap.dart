758f9dbbcc7ca3ca90a6db7b43e06d631408981a
Abdallh-Oudaa <145578823+Abdallh-Oudaa@users.noreply.github.com>
2023-10-29 16:49:34 +0200

    first commit

---
+++ lib/ui/HomeTap/HomeTap.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeTap extends StatelessWidget {
+  const HomeTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.purple,
+        child: Text("hi"),)
+    );
+}}
